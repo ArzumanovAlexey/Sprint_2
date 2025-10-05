ce173a831853e113de9f8133728522e0dd8bcd74
ArzumanovAlexey <aleksey.arzumanov@mail.ru>
2025-10-05 20:35:26 +0500

    Проект 2 спринта

---
+++ task_3.py
@@ -0,0 +1,51 @@
+class PointsForPlace:
+    def __init__(self):
+        self.points = 0
+
+    def get_points_for_place(self, place):
+        if place > 100:
+            print('Баллы начисляются только первым 100 участникам')
+            self.points = 0
+        elif place < 1:
+            print('Спортсмен не может занять нулевое или отрицательное место')
+            self.points = 0
+        else:
+            self.points = 101 - place
+        return self.points
+
+
+class PointsForMeters:
+    def __init__(self):
+        self.points = 0
+
+    def get_points_for_meters(self, meters):
+        if meters < 0:
+            print('Количество метров не может быть отрицательным')
+            self.points = 0
+        else:
+            self.points = meters * 0.5
+        return self.points
+
+
+class TotalPoints(PointsForPlace, PointsForMeters):
+    def __init__(self):
+        PointsForPlace.__init__(self)
+        PointsForMeters.__init__(self)
+        self.total = 0
+
+    def get_total_points(self, meters, place):
+        points_place = PointsForPlace.get_points_for_place(self, place)
+        points_meters = PointsForMeters.get_points_for_meters(self, meters)
+        self.total = points_place + points_meters
+        return self.total
+
+points_for_place = PointsForPlace()
+print(points_for_place.get_points_for_place(10))
+
+points_for_meters = PointsForMeters()
+print(points_for_meters.get_points_for_meters(10))
+
+total_points = TotalPoints()
+print(total_points.get_points_for_place(10))
+print(total_points.get_points_for_meters(10))
+print(total_points.get_total_points(100, 10))
